5052229861a856661ad370a7c196ba90d7cf1d7e
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-13 14:50:32 -0300

    Desenvolvimento
    Usando o get.ElementById para puxar elementos das classes que foram definidas para carregar dados do usuário.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-function fnCarregarDados(){
-const url = "https://api.github.com/users/guilherme-costa14"
-fetch(url)
-    .then(resposta => {
-        return resposta.json()
-    })
-    .then(dados=>{
-        console.dir(dados)
-    })
+function fnCarregarDados() {
+    const url = "https://api.github.com/users/guilherme-costa14"
+    fetch(url)
+        .then(resposta => {
+            return resposta.json()
+        })
+        .then(dados => {
+            console.dir(dados)
+        })
 }
 
+function fnMostrarDados(foto, nome, resumo, email, twitter, github, blog) {
+    document.getElementById("foto").src = foto
+    document.getElementById("nome").innerHTML = nome
+    document.getElementById("resumo").innerHTML = resumo + "<br><strong id='email'></strong>"
+    document.getElementById("email").innerHTML = email
+    document.getElementById("twitter").href = twitter
+    document.getElementById("github").href = github
+    document.getElementById("blog").href = blog
+}
+
+let botao = document.getElementById("botao-carregar-dados")
+botao.addEventListener("click", function () {
+    fnMostrarDados("foto.jgp", "Guilherme Costa", "Aluno", "teste@gmail.com", "https://x.com", "https://github.com", "https://dev.to")
+})
